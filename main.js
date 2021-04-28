847437741f2bb06a038513ac4ce60cc0c3dc3f0b
harikrishnadivi08 <harikrishna.divi08@gmail.com>
2021-04-28 13:46:01 -0400

    read more and read less buttons added

---
+++ main.js
@@ -0,0 +1,74 @@
+function readBtnFnctn(){
+    var dots = document.getElementById('dots');
+    var fullText = document.getElementById('fullText');
+    var readMorebtn = document.getElementById('readMoreBtn');
+
+    if(dots.style.display === 'none')
+    {
+        dots.style.display = 'inline';
+        fullText.style.display = 'none';
+        readMorebtn.innerHTML = "Read more";
+    }
+    else{
+        dots.style.display = 'none';
+        fullText.style.display = 'inline';
+        readMorebtn.innerHTML = "Read less";
+        }
+}
+
+
+function wdReadBtnFnctn(){
+    var dots = document.getElementById('wddots');
+    var fullText = document.getElementById('wdfullText');
+    var readMorebtn = document.getElementById('wdreadMoreBtn');
+
+    if(dots.style.display === 'none')
+    {
+        dots.style.display = 'inline';
+        fullText.style.display = 'none';
+        readMorebtn.innerHTML = "Read more";
+    }
+    else{
+        dots.style.display = 'none';
+        fullText.style.display = 'inline';
+        readMorebtn.innerHTML = "Read less";
+        }
+}
+
+
+function dmreadBtnFnctn(){
+    var dots = document.getElementById('dmdots');
+    var fullText = document.getElementById('dmfullText');
+    var readMorebtn = document.getElementById('dmreadMoreBtn');
+
+    if(dots.style.display === 'none')
+    {
+        dots.style.display = 'inline';
+        fullText.style.display = 'none';
+        readMorebtn.innerHTML = "Read more";
+    }
+    else{
+        dots.style.display = 'none';
+        fullText.style.display = 'inline';
+        readMorebtn.innerHTML = "Read less";
+        }
+}
+
+
+function gdreadBtnFnctn(){
+    var dots = document.getElementById('gddots');
+    var fullText = document.getElementById('gdfullText');
+    var readMorebtn = document.getElementById('gdreadMoreBtn');
+
+    if(dots.style.display === 'none')
+    {
+        dots.style.display = 'inline';
+        fullText.style.display = 'none';
+        readMorebtn.innerHTML = "Read more";
+    }
+    else{
+        dots.style.display = 'none';
+        fullText.style.display = 'inline';
+        readMorebtn.innerHTML = "Read less";
+        }
+}
